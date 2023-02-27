61d6e1905f9d0b2e35c193a79a24e2a66c9ba73b
Junior <eecfredes@gmail.com>
2023-02-26 21:41:27 -0300

    :rocket:feat: CRUD - User.controller | getAll, getById

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('users')
 export class UserController {
   @Post()
   async create(@Body() body) {
     return body;
+  }
+
+  @Get()
+  async read() {
+    return { users: [] };
+  }
+
+  @Get(':id')
+  async readOne(@Param() params) {
+    return { users: {}, params };
   }
 }
