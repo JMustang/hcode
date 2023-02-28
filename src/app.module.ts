844e47553b995254c929a5d1a854846f82b127a0
Junior <eecfredes@gmail.com>
2023-02-27 21:09:30 -0300

    :rocket:feat: DTO`s - class-validator, class-transformer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
+import { UserModule } from './user/user.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
+  exports: [AppService],
 })
 export class AppModule {}
